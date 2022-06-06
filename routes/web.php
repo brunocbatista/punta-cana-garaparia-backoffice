fc2cffd8b9085720b0c71b7ef5fcfd94c8265af8
Bruno Batista <bruncbatista@gmail.com>
2022-06-05 22:05:54 -0300

    adjustments for example test

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,15 @@ use Inertia\Inertia;
 |
 */
 
-Route::redirect('/', 'login');
+Route::get('/', function () {
+    return Inertia::render('Welcome',
+        [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+});
 
 Route::middleware([
     'auth:sanctum',
